98f3775b7beba9b88b979de48397e68a08f9f541
CesarRD-98 <dedioscesar12@gmail.com>
2025-10-01 22:41:02 -0600

    pagina de contaco, terminada

--- portafolio/src/app/(pages)/contact/page.tsx
+++ portafolio/src/app/(pages)/contact/page.tsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import './contac-page.scss'
+import {FaPhone, FaEnvelope} from 'react-icons/fa6'
+import {FaLinkedin} from 'react-icons/fa'
 
 export default function page() {
     const phoneNumber: string = '98435672'
@@ -8,27 +11,31 @@ export default function page() {
     const phoneNumberFormat: string = phoneNumber.replace(/\B(?=(\d{4})+(?!\d))/g, "-")
 
     return (
-        <div>
-            <h1>Contáctame</h1>
-            <p>
+        <section className='section'>
+            <h1 className='title'>Contáctame</h1>
+            <p className='description'>
                 Si quieres ponerte en contacto conmigo, dejo los siguientes medios para que te comuniques conmigo
                 directamente
             </p>
-            <br/>
-            <div className="card">
-                <h4>Teléfono</h4>
-                <p>+504 {phoneNumberFormat}</p>
+            <div className='card-container'>
+                <div className="card">
+                    <h4>Teléfono</h4>
+                    <FaPhone size={24}/>
+                    <p>+504 {phoneNumberFormat}</p>
+                </div>
+                <br/>
+                <div className="card">
+                    <h4>LinkedIn</h4>
+                    <FaLinkedin size={24}/>
+                    <p>{profileLinkedIn}</p>
+                </div>
+                <br/>
+                <div className="card">
+                    <h4>Correo</h4>
+                    <FaEnvelope size={24}/>
+                    <p>{email}</p>
+                </div>
             </div>
-            <br/>
-            <div className="card">
-                <h4>LinkedIn</h4>
-                <p>{profileLinkedIn}</p>
-            </div>
-            <br/>
-            <div className="card">
-                <h4>Correo</h4>
-                <p>{email}</p>
-            </div>
-        </div>
+        </section>
     )
 }
--- portafolio/src/app/(pages)/layout.tsx
+++ portafolio/src/app/(pages)/layout.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Navbar from '../components/navbar/navbar'
 import Footer from '../components/footer/footer'
+import {ChildrenModel} from "@/app/model/children.model";
 
-export default function layout({ children }: { children: React.ReactNode }) {
+export default function layout({children}: ChildrenModel) {
     return (
         <>
-            <Navbar />
+            <Navbar/>
             {children}
-            <Footer />
+            <Footer/>
         </>
     )
 }
