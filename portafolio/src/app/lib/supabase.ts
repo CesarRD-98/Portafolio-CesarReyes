a4bc15eb6769c05f9b4657a9330a1e387df148d8
CesarRD-98 <dedioscesar12@gmail.com>
2026-03-07 12:36:28 -0600

    Environment variables

--- portafolio/src/app/lib/supabase.ts
+++ portafolio/src/app/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_KEY!
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL_D!
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_KEY_D!
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
